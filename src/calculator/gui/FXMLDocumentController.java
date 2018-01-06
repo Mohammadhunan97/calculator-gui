047779af5b2fb9476e03a2ca56dadabda12f29a9
mohammad <you@example.com>
2018-01-06 17:19:12 -0500

    finished four function functionality with gui calculator

--- src/calculator/gui/FXMLDocumentController.java
+++ src/calculator/gui/FXMLDocumentController.java
@@ -11,34 +11,89 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.text.Text;
-
+import javafx.scene.control.*;
 
 /**
  *
  * @author mohammad
  */
 public class FXMLDocumentController implements Initializable {
-    private double num1, num2, num3;
-    private boolean operatorIsChosen;
-    private char myOperator;
-    private Text results; 
+    public double num1, num2, num3;
+    public String tempNum1 = "";
+    public String tempNum2 = "";
+    public String tempNum3 = "";
+    public boolean operatorIsChosen = false;
+    public String myOperator;
+    public Text results; 
+    
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
+    private void handleClear(ActionEvent event) {
+        num1 = 0;
+        num2 = 0;
+        num3 = 0;
+        tempNum1 = "";
+        tempNum2 = "";
+        tempNum3 = "";
+        operatorIsChosen = false;
+        myOperator = "";
+        results.setText("");
     }
     @FXML
-    private void getNum(double clickedNumber ){
-        if(operatorIsChosen == true){
-            num2 = clickedNumber;
+    private void handleGetNum(ActionEvent event){
+        Button button = (Button) event.getSource();
+        String type = button.getText();
+        
+        if(operatorIsChosen == false) {
+            tempNum1 += type;
+            results.setText(tempNum1);
         }else{
-            num1 = clickedNumber;
+            tempNum2 += type;
+            num2 = Double.parseDouble(tempNum2);
+            results.setText(tempNum1 + myOperator + tempNum2);
         }
-        results.setText(Double.toString(clickedNumber));
+        
+    }
+    @FXML 
+    private void handleOperator(ActionEvent event){
+        /* for this method */
+        Button button = (Button) event.getSource();
+        String type = button.getText();
+        
+        /* for operation */
+        operatorIsChosen = true;
+        myOperator = type;
+                
+        /* for users gui*/  
+        num1 = Double.parseDouble(tempNum1);
+        results.setText(tempNum1 + myOperator);
+    }
+    @FXML
+    private void handleSolution(ActionEvent event){
+        
+        if(myOperator.equals("+")){
+            num3 = num1 + num2;
+            results.setText(Double.toString(num3));
+            
+        }else if(myOperator.equals("-")){
+            num3 = num1 - num2;
+            results.setText(Double.toString(num3));
+            
+        }else if(myOperator.equals("*")){
+            num3 = num1 * num2;
+            results.setText(Double.toString(num3));
+            
+        }else if(myOperator.equals("/")){
+            num3 = num1 / num2;
+            results.setText(Double.toString(num3));           
+        }else{
+            System.out.println("wtf");
+        }
     }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+       results.setText(" ");
     }    
     
 }
